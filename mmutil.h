bacbc647032576b9eea6c517b22414b76c4f7d7e
Mario Carneiro <di.gama@gmail.com>
2022-01-01 16:17:01 -0800

    remove mmutil.{c, h} (#24)

--- mmutil.h
+++
@@ -1,7 +0,0 @@
-/*****************************************************************************/
-/*        Copyright (C) 2005  NORMAN MEGILL  nm at alum.mit.edu              */
-/*            License terms:  GNU General Public License                     */
-/*****************************************************************************/
-/*34567890123456 (79-character line to adjust editor window) 2345678901234567*/
-
-/* (This file is currently empty.) */
