3fc8481c7dd59757b32e2edc6b2198a17731903f
Mario Carneiro <di.gama@gmail.com>
2022-01-07 05:05:55 -0500

    add BUILD command

---
+++ tools/build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Shell script equivalent of Metamath BUILD tool
+# Mario Carneiro 7-Jan-2022
+
+usage() {
+  cat << HELP
+BUILD - Build a file with multiple tokens per line from a list
+
+This command combines a list of tokens into multiple tokens per line,
+as many as will fit per line, separating them with spaces.
+Syntax:  build.sh < INFILE > OUTFILE
+HELP
+}
+
+if [ $# -eq 1 ] && [ "$1" = "-h" ]; then usage; exit; fi
+if [ $# -ne 0 ]; then usage; exit 1; fi
+
+awk '{
+  if ($0 != "") {
+    if (out == "")
+      out = $0;
+    else if (length(out) + length($0) > 72) {
+      print out;
+      out = $0;
+    } else {
+      out = out " " $0;
+    }
+  }
+} END {
+  if (out != "") print out;
+}'
