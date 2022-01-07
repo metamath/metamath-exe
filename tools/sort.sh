d0a2901d93e38c9040fbb787e5681e02824ce580
Mario Carneiro <di.gama@gmail.com>
2022-01-07 06:03:15 -0500

    add SORT command

---
+++ tools/sort.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Shell script equivalent of Metamath SORT tool
+# Mario Carneiro 7-Jan-2022
+
+usage() {
+  cat << HELP
+SORT - Sort the lines in a file with key starting at specified string
+
+This command sorts a file, comparing lines starting at a key string.
+If the key string is blank, the line is compared starting at column 1.
+If a line doesn't contain the key, it is compared starting at column 1.
+Syntax:  sort.sh KEY < INFILE > OUTFILE
+HELP
+}
+
+if [ $# -eq 1 ] && [ "$1" = "-h" ]; then usage; exit; fi
+if [ $# -ne 1 ]; then usage; exit 1; fi
+
+if [ "$1" != "" ]; then echo "SORT with key unsupported"; exit 2; fi
+
+sort
