9dac482fe944f7f851be0ad18e0133c20adc0be0
Mario Carneiro <di.gama@gmail.com>
2022-01-08 06:54:05 -0500

    use seq in number.sh

--- tools/number.sh
+++ tools/number.sh
@@ -6,8 +6,8 @@ usage() {
   cat << HELP
 NUMBER - Create a list of numbers
 
-This command creates a list of numbers.  Hint:  Use the RIGHT command to
-right-justify the list after creating it.
+Print numbers from FIRST to LAST, in steps of INCR.
+Hint:  Use the RIGHT command to right-justify the list after creating it.
 Syntax:  number.sh FIRST LAST INCR > OUTFILE
 HELP
 }
@@ -18,8 +18,4 @@ if [ $# -ne 3 ] || ! isNum "$1" || ! isNum "$2" || ! isNum "$3"; then
   usage; exit 1
 fi
 
-x=$1
-while [ $x -le $2 ]; do
-  echo $x
-  x=$(( $x + $3 ))
-done
+seq $1 $3 $2
