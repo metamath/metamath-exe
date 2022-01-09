3459296b944d3a9ca28069ea0a9a05bc9c08d81c
Mario Carneiro <di.gama@gmail.com>
2022-01-09 00:36:10 -0500

    alternative for gensub()

--- tools/substitute.sh
+++ tools/substitute.sh
@@ -22,9 +22,10 @@ if [ $# -eq 1 ] && [ "$1" = "-h" -o "$1" = "--help" ]; then usage; exit; fi
 if [ $# -ne 4 ]; then usage; exit 1; fi
 case "$3" in
   A | a)  all=1;;
-  '' | '0' | *[!0-9]*)  usage; exit 1;;
+  '' | *[!0-9]*)  usage; exit 1;;
   *)  occs=$3;;
 esac
+if [ "$all" = "" ] && [ $occs -eq 0 ]; then usage; exit 1; fi
 
 # multiline oldStr not supported
 awk -v oldStr="$1" -v newStr="$2" -v matchStr="$4" \
@@ -35,7 +36,20 @@ awk -v oldStr="$1" -v newStr="$2" -v matchStr="$4" \
 } {
   if (index($0, matchStr)) {
     if (all) gsub(oldStr, newStr);
-    else gensub(oldStr, newStr, occs + 1);
+    else {
+      # non-gawk version of:
+      # $0 = gensub(oldStr, newStr, occs + 1);
+
+      lhs = "";
+      for (i = occs; i > 1; i--) {
+        if (!match($0, oldStr)) break;
+        p = RSTART + RLENGTH;
+        lhs = lhs substr($0, 1, p - 1);
+        $0 = substr($0, p);
+      }
+      if (i == 1) sub(oldStr, newStr);
+      $0 = lhs $0;
+    }
   }
   print;
 }'
