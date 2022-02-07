7c97d7dae048b50f73c3546d539ebe1013228bc8
Jim Kingdon <kingdon@panix.com>
2022-02-07 07:53:46 -0800

    Add some of the tests from metamath-test
    - Does not include anything which took a long time to run
    - Does not include versions of set.mm and the like - we check those
    elsewhere
    - Emphasis is on failure tests

---
+++ tests/demo0-includer.mm
@@ -0,0 +1,23 @@
+$( demo0-includer.mm  1-Jan-04 $)
+
+$(
+                      PUBLIC DOMAIN DEDICATION
+
+This file is placed in the public domain per the Creative Commons Public
+Domain Dedication. http://creativecommons.org/licenses/publicdomain/
+
+Norman Megill - email: nm at alum.mit.edu
+
+$)
+
+
+$[ demo0-includee.mm $]
+
+$( Prove a theorem $)
+    th1 $p |- t = t $=
+  $( Here is its proof: $)
+       tt tze tpl tt weq tt tt weq tt a2 tt tze tpl
+       tt weq tt tze tpl tt weq tt tt weq wim tt a2
+       tt tze tpl tt tt a1 mp mp
+     $.
+
