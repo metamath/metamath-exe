817b7e654c3572f7ad71c0db3e766105ddf43957
Jim Kingdon <kingdon@panix.com>
2022-02-07 21:04:03 -0800

    Add some of the tests from metamath-test (#57)
    - Does not include anything which took a long time to run
    - Does not include versions of set.mm and the like - we check those
    elsewhere
    - Emphasis is on failure tests

---
+++ tests/anatomy-bad1.mm
@@ -0,0 +1,30 @@
+$( anatomy.mm $)
+
+$(
+                      PUBLIC DOMAIN DEDICATION
+
+This file is placed in the public domain per the Creative Commons Public
+Domain Dedication. http://creativecommons.org/licenses/publicdomain/
+
+Norman Megill - email: nm at alum.mit.edu
+
+$)
+
+
+$( This file is the introductory formal system example described
+   in Chapter 4 of the Metamath book, "Anatomy of a proof". $)
+
+$( Declare the constant symbols we will use $)
+$c ( ) -> wff $.
+$( Declare the metavariables we will use $)
+$v p q r s $.
+$( Declare wffs $)
+wp $f wff p $.
+wq $f wff q $.
+wr $f wff r $.
+ws $f wff s $.
+w2 $a wff ( p -> q ) $.
+
+$( Prove a simple theorem. $)
+wnew $p wff ( s -> ( r -> p ) ) $= ws wr wp w2 ws $.
+
