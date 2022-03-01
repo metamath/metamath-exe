2a5c4702a3899b4bd133189b4e8d5d7d95b9b046
Wolf Lammen <wolf.lammen@gmail.com>
2022-03-01 16:39:01 +0100

    be more precise about the limitations of seg

--- src/mmvstr.h
+++ src/mmvstr.h
@@ -364,23 +364,23 @@ int linput(FILE *stream, const char *ask, vstring *target);
  * \param[in] sin (not null) pointer to the NUL-terminated source text.
  * \param[in] p1 offset of the first byte of the substring, counted in bytes from
  *   the first one of __sin__, a 1-based index.  A value less than 1 is
- *   internally corrected to 1, but it must never point to or beyond the
- *   terminating NUL of __sin__.
+ *   internally corrected to 1, but it must not point beyond the terminating
+ *   NUL of __sin__, if __p1__ <= __p2__.
  * \param[in] p2 offset of the last byte of the substring, counted in bytes from
  *   the first one of __sin__, a 1-based index.  The natural bounds of this
  *   value are __p1__ - 1 and the length of __sin__.  Values outside of this
- *   range are corrected to the closer of these limits.  If __p2__ < __p1__ the
- *   empty string is returned.
+ *   range are internally corrected to the closer of these limits.  If __p2__ <
+ *   __p1__ the empty string is returned.
  * \attention the indices are 1-based: seg("hello", 2, 3) == "el"!
  * \return a pointer to new allocated \a temp_vstring referencing the
  *   requested substring, that is also pushed onto the top of \a tempAllocStack
  * \pre
- *   __p__ points to a character of __sin__ before the terminating NUL
- *   character.
+ *   EITHER: __p1__ points to a character within __sin__, including the
+ *   terminating NUL, OR: __p2__ < __p1__.
  * \post
  *   A pointer to the substring is pushed on \a tempAllocStack, even if it
  *   empty;
- * \bug a stack overflow of \a tempAllocStack is not handled correctly.
+ * \bug a stack overflow of \a tempAllocStack is not handled correctly;
  */
 temp_vstring seg(const char *sin, long p1, long p2);
 /*!
@@ -390,7 +390,7 @@ temp_vstring seg(const char *sin, long p1, long p2);
  * \param[in] sin (not null) pointer to the NUL-terminated source text.
  * \param[in] p offset of the substring in bytes from the first byte of __sin__,
  *   1-based.  A value less than 1 is internally corrected to 1, but it must
- *   not point to or beyond the terminating NUL of __sin__.
+ *   never point beyond the terminating NUL of __sin__.
  * \param l length of substring in bytes.  Negative values are corrected to 0.
  *   If __p__ + __l__ exceeds the length of __sin__, then only the portion up
  *   to the terminating NUL is taken.
@@ -403,7 +403,11 @@ temp_vstring seg(const char *sin, long p1, long p2);
  * \post
  *   A pointer to the substring is pushed on \a tempAllocStack, even if it
  *   empty;
- * \bug a stack overflow of \a tempAllocStack is not handled correctly.
+ * \bug
+ *   - a stack overflow of \a tempAllocStack is not handled correctly;
+ *   - If __p__ addresses a character beyond the terminating NUL of __sin__,
+ *     the resulting string is undefined, and a memory access violation is
+ *     possible.
  */
 temp_vstring mid(const char *sin, long p, long l);
 /*!
@@ -436,7 +440,7 @@ temp_vstring left(const char *sin, long n);
  *
  * \param[in] sin (not null) pointer to a NUL terminated string to be copied
  *   from.
- * \param[in] n 1-based position of the first character not skipped at the
+ * \param[in] n 1-based index of the first not skipped character at the
  *   beginning of the source.  The natural bounds of this value is 1 and the
  *   length of __sin__ + 1 in bytes.  Any value outside of these bounds is
  *   corrected to the closer one of these bounds.
