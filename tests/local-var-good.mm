9e0cece3dac1480fc6c01912834bffe86ae9fe7a
Jim Kingdon <kingdon@panix.com>
2022-05-08 08:08:49 -0700

    Add test for missing $f (#83)
    This is based on an observation by Glauco on the metamath mailing list
    and is taken from the relevant lines of set.mm.

---
+++ tests/local-var-good.mm
@@ -0,0 +1,14 @@
+  $c formula $.
+  $c A. $.
+  $c setvar $.
+  $v ph $.
+  wph $f formula ph $.
+
+  ${
+    $v x $.
+    $( Let ` x ` be an individual variable (temporary declaration). $)
+    vx.wal $f setvar x $.
+    $( Extend formula definition to include the universal quantifier ('for all').  $)
+    wal $a formula A. x ph $.
+  $}
+
