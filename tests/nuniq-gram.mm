d15bf6748908beeb93acd7bf880d037a59a347a0
Jim Kingdon <kingdon@panix.com>
2022-06-04 20:02:19 -0700

    Add example of a non-unique grammar from the mailing list.

---
+++ tests/nuniq-gram.mm
@@ -0,0 +1,11 @@
+$( Here we have two variable typecodes A and B, and one logical 
+   typecode |-, and the question is whether |- connects to A or B.
+   It parses either way. This is actually an unambiguous formal system,
+   because we require only that there are no two ways to derive the proof
+   of the *same* syntax theorem, and "A *" and "B *" are different theorems. $)
+$c A B |- * $. $v a b $.
+va $f A a $.
+vb $f B b $.
+as $a A * $.
+bs $a B * $.
+ax $a |- * $.
