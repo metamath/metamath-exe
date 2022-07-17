c8853973aaf12ff2421addea47890fbcde15b3f6
Wolf Lammen <wolf.lammen@gmail.com>
2022-07-17 17:12:44 +0200

    make exitFatalError more readable

--- src/mmfatl.c
+++ src/mmfatl.c
@@ -481,31 +481,41 @@ int setFatalErrorMessage(FatalErrorFormat format, ...)
     return ok;
 }
 
-void exitFatalError(
-    unsigned line,
-    char const* file,
-    FatalErrorFormat messageFormat, ...)
+/*!
+ * empties the error buffer and fills it with location information.
+ * If not successful, the buffer remains in an empty state.
+ * \param line program line causing the error, or 0 if not available.
+ * \param file file containing the faulting line, or NULL if not available.
+ * \returns a pointer to the end of the buffer contents.
+ */
+static char* setLocationData(unsigned line, char const* file)
 {
+    char* result = fatalErrorBufferBegin();
     int hasPosInfo = line != 0 || (file != NULL && *file != NUL);
     if (hasPosInfo)
     {
         char const* posFormat = line == 0? "%s:\n" : "%s@%u:\n";
         hasPosInfo = setFatalErrorMessage(posFormat, file, line);
     }
-    // now process the message
+    if (hasPosInfo)
+        result += strlen(result);
+    else
+       clearFatalErrorBuffer();
+    
+    return result;
+}
+
+void exitFatalError(
+    unsigned line,
+    char const* file,
+    FatalErrorFormat messageFormat, ...)
+{
     struct ParserState state;
     resetParserState(&state, messageFormat);
 
-    if (hasPosInfo)
-    {
-        char* bufferStart = fatalErrorBufferBegin();
-        // continue parsing with the message
-        state.formatPos = messageFormat;
-        state.buffer = bufferStart + strlen(bufferStart);
-    }
-    else
-        clearFatalErrorBuffer();
+    state.buffer = setLocationData(line, file);
 
+    // now process the message
     va_start(state.args, messageFormat);
     appendMessage(&state); 
     va_end(state.args);
