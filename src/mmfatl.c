091f5a0a4fedbda98a049dd924251e527b7ae731
Wolf Lammen <wolf.lammen@gmail.com>
2022-10-25 21:38:02 +0200

    fix typo

--- src/mmfatl.c
+++ src/mmfatl.c
@@ -85,7 +85,7 @@ static char const* unsignedToString(unsigned long value) {
   /*
    * sizeof(unsigned long) * CHAR_BIT are the bits available in an unsigned long,
    * the factor 146/485, derived from a chained fraction, is about 0.3010309 or log 2.
-   * So the number within the bracket is the number of decimal digits encodable
+   * So the number within the brackets is the number of decimal digits encodable
    * in an unsigned long.  Two extra bytes compensate the truncation error and
    * leave space for a terminal NUL character.
    */
