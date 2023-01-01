f9a35b3155d6c1447a20f0e1717121341ff54eed
Wolf Lammen <30736367+wlammen@users.noreply.github.com>
2023-01-01 22:37:46 +0100

    dead code elimination in mmfatl (#115)
    * dead code elimination
    
    * forgot the header file
    
    Co-authored-by: Wolf Lammen <wolf.lammen@gmail.com>

--- src/mmfatl.h
+++ src/mmfatl.h
@@ -127,25 +127,6 @@ enum fatalErrorPlaceholderType {
 
 
 /*!
- * \brief grammar support: generate a placeholder token for insertion into a
- *   format string.
- *
- * The placeholders in fatal errors are a subset of those used in the C library
- * function printf.  A flexible implementation might want to query placeholder
- * tokens during an automatic message generation, rather than hardcoding them
- * directly in the format string.
- * \param aType data type of the value replacing the placeholder in a format
- *   string.  \ref MMFATL_PH_PREFIX is allowed as a type, yielding a token
- *   standing for the character \ref MMFATL_PH_PREFIX itself.
- * \return a placeholder of a supported type, or NULL, if you somehow
- *   manage to dodge C type checking.
- * \attention the result is stable only until the next call to this
- *   function.
- */
-extern char const* getFatalErrorPlaceholderToken(
-                enum fatalErrorPlaceholderType aType);
-
-/*!
  * \brief Prepare internal data structures for an error message.
  * 
  * Empties the message buffer used to construct error messages by
