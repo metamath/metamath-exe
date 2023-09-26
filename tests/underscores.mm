7f3476647d9d3cd539aef993fa602f60c540ce81
Jim Kingdon <kingdon@panix.com>
2023-09-25 23:00:03 -0700

    Add tests for underscores in URLs
    This is existing behavior but it would seem to make sense because the
    concept of italics or subscripts does not make sense within a URL.

--- tests/underscores.mm
+++ tests/underscores.mm
@@ -1,7 +1,10 @@
   $c a $.
 
   $( This is _italic_ and the command "MM-PA> MINIMIZE__WITH *" and a
-     sub_script. $)
+     sub_script.
+
+     In URLs underscores stand for themselves and a double underscore
+     is two underscores: ~ https://example.com/one_two__three $)
   underscores $a a $.
 
 $( $t
