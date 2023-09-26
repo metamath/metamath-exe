7887378cb47931d2a54f89d323875345994cb7a6
Mario Carneiro <di.gama@gmail.com>
2023-09-26 05:06:22 -0400

    test underscores in HTML

--- tests/underscores.mm
+++ tests/underscores.mm
@@ -1,7 +1,13 @@
   $c a $.
 
   $( This is _italic_ and the command "MM-PA> MINIMIZE__WITH *" and a
-     sub_script, a label ~ lab__1_2 , and a url: ~ http://a_b__c.com. $)
+     sub_script, a label ~ lab__1_2 , and a url: ~ http://a_b__c.com.
+     <HTML>
+     Inside HTML tags:
+
+     This is _italic_ and the command "MM-PA> MINIMIZE__WITH *" and a
+     sub_script, a label ~ lab__1_2 , and a url: ~ http://a_b__c.com.
+     </HTML> $)
   underscores $a a $.
 
 $( $t
