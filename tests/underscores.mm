c2f34ba86519068f65d20812feaac07dffcaa168
Mario Carneiro <di.gama@gmail.com>
2023-09-28 02:56:21 +0200

    undouble `[` even if there is no bibliography (#163)

--- tests/underscores.mm
+++ tests/underscores.mm
@@ -1,12 +1,14 @@
   $c a $.
 
   $( This is _italic_ and the command "MM-PA> MINIMIZE__WITH *" and a
-     sub_script, a label ~ lab__1_2 , and a url: ~ http://a_b__c.com.
+     sub_script, a label ~ lab[[``~~__ , and a url: ~ http://a_b__c.com/[[``~~__.
+     Doubled chars: [[ `` ~~ __
      <HTML>
      Inside HTML tags:
 
      This is _italic_ and the command "MM-PA> MINIMIZE__WITH *" and a
-     sub_script, a label ~ lab__1_2 , and a url: ~ http://a_b__c.com.
+     sub_script, a label ~ lab[[``~~__ , and a url: ~ http://a_b__c.com[[``~~__.
+     Doubled chars: [[ `` ~~ __
      </HTML> $)
   underscores $a a $.
 
